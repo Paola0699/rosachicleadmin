069f89f9c23f5304eb776e51cf5e5a08e96714b3
kevin <keveggts@gmail.com>
2021-01-17 23:32:21 -0600

    forcing https

--- server.js
+++ server.js
@@ -11,23 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
    //forcing use of https
-   /* app.use(function(req, res, next) {
+   app.use(function(req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') 
       return res.redirect('https://'+ req.headers.host + req.url);
     else
       return next();
-  }); */
+  });
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Handle React routing, return all requests to React app
-  /* app.get('*', function(req, res) {
+  app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  }); */
+  });
 }
-
-app.get('/.well-known/acme-challenge/:content',function(req,res){
+//get SSL
+/* app.get('/.well-known/acme-challenge/:content',function(req,res){
   res.send('QqMl0ojsd9qfuvC75g0cd3kF5yHrVkkagBWag-oHEB0.N4tbdafRM11q63l9g2wR5R06hLGFmj5uh7GDEl21n9s');
-})
+}) */
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
