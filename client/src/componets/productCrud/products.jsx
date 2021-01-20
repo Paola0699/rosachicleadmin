93930344ac33de1a942a6b9159f34144538d7246
Paola0699 <paola.pv1999@gmail.com>
2021-01-19 22:09:07 -0600

    Products table

--- client/src/componets/productCrud/products.jsx
+++ client/src/componets/productCrud/products.jsx
@@ -17,20 +17,6 @@ const columns = [
     },
 
     {
-        name: 'Descripción',
-        selector: 'year',
-        sortable: true,
-        right: true,
-    },
-
-    {
-        name: 'Calorias',
-        selector: 'year',
-        sortable: true,
-        right: true,
-    },
-
-    {
         name: 'Costo',
         selector: 'year',
         sortable: true,
@@ -47,10 +33,63 @@ const columns = [
     {
         name: 'Acciones',
         selector: 'year',
-        sortable: true,
+        cell: row => <div className='is-flex'><button className='button is-success' style={{ marginRight: '2%' }}>Detalles</button><button className='button is-success is-outlined'>Eliminar</button></div>,
         right: true,
     },
 ];
+
+const customStyles = {
+    header: {
+        style: {
+            fontSize: '22px',
+            color: 'white',
+            backgroundColor: '#e91e63',
+            minHeight: '56px',
+            paddingLeft: '16px',
+            paddingRight: '8px',
+        },
+    },
+    headRow: {
+        style: {
+            backgroundColor: '#fafafa',
+            minHeight: '56px',
+            borderBottomWidth: '1.5px',
+            borderBottomColor: '#1293e1',
+            borderBottomStyle: 'solid',
+        },
+        denseStyle: {
+            minHeight: '32px',
+        },
+    },
+    headCells: {
+        style: {
+            fontSize: '1rem',
+            fontWeight: 700,
+            color: '#616161',
+            paddingLeft: '16px',
+            paddingRight: '16px',
+        },
+        activeSortStyle: {
+            color: '#1293e1',
+            '&:focus': {
+                outline: 'none',
+            },
+            '&:hover:not(:focus)': {
+                color: '#1293e1',
+            },
+        },
+        inactiveSortStyle: {
+            '&:focus': {
+                outline: 'none',
+                color: '#1293e1',
+            },
+            '&:hover': {
+                color: '#4dbbff',
+            },
+        },
+    },
+};
+
 
 function Products() {
     return (
@@ -67,9 +106,31 @@ function Products() {
             </section>
             <section class="section">
                 <div class="container">
+                    <div className='columns'>
+                        <div className='column is-8'></div>
+                        <div className='column is-4'>
+                            <div class="field has-addons">
+                                <div class="control is-expanded">
+                                    <div class="select is-fullwidth">
+                                        <select name="country">
+                                            <option value="Argentina">Juice</option>
+                                            <option value="Bolivia">Smoothies</option>
+                                            <option value="Brazil">Strongthies</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="control">
+                                    <button type="submit" class="button is-primary">Choose</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <DataTable
                         columns={columns}
                         data={data}
+                        pagination={true}
+                        customStyles={customStyles}
+                        paginationComponentOptions={{ rowsPerPageText: 'Filas por pagina:', rangeSeparatorText: 'de' }}
                     />
                 </div>
             </section>
