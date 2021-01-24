ba510e9dae96deacf3a48c71b86b78b4538e7904
kevin <keveggts@gmail.com>
2021-01-23 22:47:25 -0600

    pay method

--- client/src/componets/saleCrud/newsale.jsx
+++ client/src/componets/saleCrud/newsale.jsx
@@ -1,9 +1,14 @@
 import Navbar from "../common/navbar"
 import Breadcrum from "../common/breadcrum"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
-import { useEffect, useState } from 'react'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useRef, useState } from 'react'
 import firebase from '../../firebaseElements/firebase'
+import { Modal } from 'react-responsive-modal'
+import 'react-responsive-modal/styles.css';
+import CurrencyFormat from 'react-currency-format';
+import Swal from 'sweetalert2'
+
 const db = firebase.firestore();
 const quantityButtonStyle = {
     backgroundColor: 'transparent',
@@ -12,11 +17,11 @@ const quantityButtonStyle = {
     fontWeight: '900',
     borderRadius: '4px 0px 0px 4px'
 }
-const productQuantityStyle = { 
-    display: 'flex', 
-    alignItems: 'center', 
-    padding: '.5rem .8rem', 
-    border: '1px solid rgb(221, 221, 221)' 
+const productQuantityStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '.5rem .8rem',
+    border: '1px solid rgb(221, 221, 221)'
 }
 
 function Newsale() {
@@ -24,6 +29,11 @@ function Newsale() {
     const [productsList, setProductsList] = useState([])
     const [filteredProductsList, setFilteredProductsList] = useState([])
     const [orderProducts, setOrderProducts] = useState([])
+    const [open, setOpen] = useState(false);
+    const [payMethod,setPayMethod] = useState('cash')
+    const cash = useRef(null);
+    const debit = useRef(null);
+    const credit = useRef(null);
 
     useEffect(() => {
         db.collection("products").onSnapshot(doc => {
@@ -49,13 +59,13 @@ function Newsale() {
     const filterProducts = filterBy => {
         setFilteredProductsList(productsList.filter(product => product.category === filterBy))
     }
-    function getCurrentOrderProducts(product){
+    function getCurrentOrderProducts(product) {
         const i = orderProducts.map(e => e.id).findIndex(ele => ele === product.id);
-        let aux = orderProducts.map(e=>e) 
-        return [i,aux];
+        let aux = orderProducts.map(e => e)
+        return [i, aux];
     }
     const addProduct = product => {
-        let [i,aux] = getCurrentOrderProducts(product)
+        let [i, aux] = getCurrentOrderProducts(product)
         if (i === -1)
             aux.push({ quantity: 1, ...product })
         else
@@ -63,19 +73,118 @@ function Newsale() {
         setOrderProducts(aux)
     }
     const moreProduct = product => {
-        let [i,aux] = getCurrentOrderProducts(product)
+        let [i, aux] = getCurrentOrderProducts(product)
         aux[i].quantity++;
         setOrderProducts(aux)
     }
     const lessProduct = product => {
-        let [i,aux] = getCurrentOrderProducts(product)
+        let [i, aux] = getCurrentOrderProducts(product)
         if (product.quantity === 1)
             aux.splice(i, 1)
         else
             aux[i].quantity--;
         setOrderProducts(aux)
     }
-    return (
+    const totalOrder = ()=>{
+        const reducer = (accumulator, product) => accumulator + (product.quantity*product.price);
+        return orderProducts.reduce(reducer,0)
+    }
+     const order = ()=>{
+        if (payMethod === 'debit' ||payMethod === 'credit') {
+            console.log('handeled')
+            Swal.fire({
+                title: '¿Pasó la tarjeta?',
+                text: "Confirma el cobro en tu terminal antes de registrar la compra",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#e91e63',
+                cancelButtonColor: '#bdbdbd ',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No'
+            })/* .then((result) => {
+                if (result.value) {
+                    let products = JSON.parse(localStorage.getItem('orderProducts'))
+                    let paymethod
+                    paymethod = cash.current.checked ? 'Efectivo' : paymethod
+                    paymethod = debit.current.checked ? 'Tarjeta de debito' : paymethod
+                    paymethod = credit.current.checked ? 'Tarjeta de credito' : paymethod
+
+
+                    let order = {
+                        car: carWord + carNumber,
+                        paymethod: paymethod,
+                        date: firebase.firestore.Timestamp.now(),
+                        pending: true,
+                        waiter: this.state.waiter,
+                        toprint: true
+                    }
+                    if (tips.current)
+                        order.tips = Number(tips.current.innerText);
+                    order.products = products;
+                    db.doc('orders').collection('orders').add(order)
+                    switchOff()
+                    close()
+                    localStorage.removeItem('orderProducts')
+                    reload()
+                    Swal.fire(
+                        '¡Ordenado!',
+                        'La orden está siendo preparada',
+                        'success'
+                    )
+                    this.setState({
+                        verifyCar: 0,
+                        verifyRow: 0
+                    })
+                }
+            }) */
+        }  else {
+            Swal.fire({
+                title: '¿El cobro fue correcto?',
+                text: "Confirma el cobro antes de registrar la compra",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonColor: '#e91e63',
+                cancelButtonColor: '#bdbdbd ',
+                confirmButtonText: 'Si',
+                cancelButtonText: 'No'
+            })/* .then((result) => {
+                if (result.value) {
+                    let products = JSON.parse(localStorage.getItem('orderProducts'))
+                    let paymethod
+                    paymethod = cash.current.checked ? 'Efectivo' : paymethod
+                    paymethod = debit.current.checked ? 'Tarjeta de debito' : paymethod
+                    paymethod = credit.current.checked ? 'Tarjeta de credito' : paymethod
+                    let order = {
+                        car: carWord + carNumber,
+                        paymethod: paymethod,
+                        date: firebase.firestore.Timestamp.now(),
+                        pending: true,
+                        waiter: this.state.waiter,
+                        toprint: true
+                    }
+                    if (tips.current)
+                        order.tips = Number(tips.current.innerText);
+                    order.products = products;
+                    db.doc('orders').collection('orders').add(order)
+                    switchOff()
+                    close()
+                    localStorage.removeItem('orderProducts')
+                    reload()
+                    Swal.fire(
+                        '¡Ordenado!',
+                        'La orden esta siendo preparada',
+                        'success'
+                    )
+                    this.setState({
+                        verifyCar: 0,
+                        verifyRow: 0
+                    })
+                }
+            })  */
+
+        }
+    } 
+    return (<>
         <div>
             <Navbar />
             <section className="hero is-primary">
@@ -164,21 +273,103 @@ function Newsale() {
                                                         <button onClick={() => moreProduct(product)} style={quantityButtonStyle}>+</button>
                                                     </div>
                                                     </td>
-                                                    <td>{product.price} </td>
-                                                    <td>{product.price*product.quantity}</td>
+                                                    <td>
+                                                        <CurrencyFormat decimalScale={2} fixedDecimalScale={true} 
+                                                        value={product.price} displayType={'text'} thousandSeparator={true} 
+                                                        prefix={'$'} />
+                                                    </td>
+                                                    <td>
+                                                        <CurrencyFormat decimalScale={2} fixedDecimalScale={true} 
+                                                        value={product.price*product.quantity} displayType={'text'} thousandSeparator={true} 
+                                                        prefix={'$'} />
+                                                    </td>
                                                 </tr>)}
                                         </table>
                                     </div>
                                 </div>
                             </div>
                             <br />
-                            <button disabled={orderProducts.length>0 ? false : true} className='button is-success is-fullwidth'>CONFIRMAR ORDEN</button>
+                            <button onClick={() => setOpen(true)} disabled={orderProducts.length > 0 ? false : true} className='button is-success is-fullwidth'>CONFIRMAR ORDEN</button>
                         </div>
                     </div>
                 </div>
             </section>
-
         </div>
-    )
+        <Modal open={open} onClose={() => setOpen(false)} center >
+            <div className="modal-header">
+                <h5 className="modal-title f-w-600" id="exampleModalLabel2">Confirmar orden</h5>
+            </div>
+            <div className="modal-body">
+                <br />
+                <div className="user-status table-responsive products-table">
+                    <table className="table table-bordernone mb-0">
+                        <thead>
+                            <tr>
+                                <th scope="col">Cantidad</th>
+                                <th scope="col">Producto</th>
+                                <th scope="col">Descripción</th>
+                                <th scope="col">Precio Unitario</th>
+                                <th scope="col">Total</th>
+                            </tr>
+                        </thead>
+                        {orderProducts.map(product =>
+                            <tr key={product.id}>
+                                <td>{product.quantity} </td>
+                                <td>{product.name} </td>
+                                <td>{product.description} </td>
+                                <td>
+                                    <CurrencyFormat decimalScale={2} fixedDecimalScale={true} 
+                                    value={product.price} displayType={'text'} thousandSeparator={true} 
+                                    prefix={'$'} />
+                                </td>
+                                <td>
+                                    <CurrencyFormat decimalScale={2} fixedDecimalScale={true} 
+                                    value={product.price*product.quantity} displayType={'text'} thousandSeparator={true} 
+                                    prefix={'$'} />
+                                </td>
+                            </tr>
+                        )}
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>Total a pagar</td>
+                            <td>
+                                <CurrencyFormat decimalScale={2} fixedDecimalScale={true} 
+                                value={totalOrder()} displayType={'text'} thousandSeparator={true} 
+                                prefix={'$'} />
+                            </td>
+                        </tr>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <div className="row" style={{ /* backgroundColor: '#f5f5f5', */ padding: '3%', marginTop: '4%' }}>
+                        <div className='col'><b>Forma de pago</b></div>
+                        <div className="col" >
+                            <label className="d-block" >
+                                <input onChange={()=>setPayMethod('cash')}  className="radio_animated" id="edo-ani3" type="radio" name="rdo-ani2" defaultChecked />Efectivo
+                            </label>
+                        </div>
+                        <div className="col" >
+                            <label className="d-block">
+                                <input onChange={()=>setPayMethod('debit')}  className="radio_animated" id="edo-ani4" type="radio" name="rdo-ani2" />Tarjeta de debito
+                            </label>
+                        </div>
+                        <div className="col" >
+                            <label className="d-block">
+                                <input onChange={()=>setPayMethod('credit')} className="radio_animated" id="edo-ani4" type="radio" name="rdo-ani2" /> Tarjeta de credito
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button onClick={order} className="button" style={{ backgroundColor: '#e91e63 ', color: 'white' }}>Ordenar</button>
+                <button className="button" onClick={() => setOpen(false)}>Cancelar</button>
+            </div>
+        </Modal>
+    </>)
 }
 export default Newsale;
